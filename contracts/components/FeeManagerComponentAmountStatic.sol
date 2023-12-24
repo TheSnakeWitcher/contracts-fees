da6ac6b2e5e8d1a2ca2803e3e3bfb56024654968
Mr.PAPI <thesnakewitcher@gmail.com>
2023-12-24 17:42:46 -0500

    feat: add default capabilities to FeeManagerComponentAmountStatic

--- contracts/components/FeeManagerComponentAmountStatic.sol
+++ contracts/components/FeeManagerComponentAmountStatic.sol
@@ -3,18 +3,34 @@ pragma solidity ^0.8.23 ;
 pragma abicoder v2;
 
 
-/**
- * @title FeeManagerComponentAmountStatic
- */
+bytes4 constant DEFAULT_OPERATION = bytes4(0) ;
+address constant DEFAULT_FEE_COLLECTOR = address(0) ;
+
+
+/// @title FeeManagerComponentAmountStatic
 abstract contract FeeManagerComponentAmountStatic {
 
     mapping(address feeCollector => uint256 feeAmount) private _feeAmount ;
 
     event FeeAmountChanged(address feeCollector, uint256 oldFeeAmount, uint256 newFeeAmount) ;
 
+    constructor(uint256 defaultFeeAmount_) {
+        _feeAmount[DEFAULT_FEE_COLLECTOR] = defaultFeeAmount_ ;
+    }
+
+    function feeAmount() public view virtual returns (uint256) {
+        return _feeAmount[DEFAULT_FEE_COLLECTOR] ;
+    }
+
     /// @dev maybe call it feesOf ?
     function feeAmount(address feeCollector) public view virtual returns (uint256) {
-        return _feeAmount[feeCollector] ;
+        uint256 fee = _feeAmount[feeCollector] ;
+        if (fee != 0) return fee ;
+        return feeAmount() ;
+    }
+
+    function _setDefaultFeeAmount(uint256 newFeeAmount) internal virtual {
+        _setFeeAmount(DEFAULT_FEE_COLLECTOR, newFeeAmount) ;
     }
 
     function _setFeeAmount(address feeCollector, uint256 newFeeAmount) internal virtual {
