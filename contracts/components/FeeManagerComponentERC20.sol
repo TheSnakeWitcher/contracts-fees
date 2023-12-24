36a63b92563658ae4aaa87d07cf343cc93a4839c
Mr.PAPI <thesnakewitcher@gmail.com>
2023-12-24 16:27:19 -0500

    feat: add defaultToken to FeeManagerComponentERC20

--- contracts/components/FeeManagerComponentERC20.sol
+++ contracts/components/FeeManagerComponentERC20.sol
@@ -9,13 +9,20 @@ import "@openzeppelin/contracts/utils/Context.sol" ;
 
 contract FeeManagerComponentERC20 is Context {
 
+    IERC20 private _defaultFeeToken ;
     mapping(address feeCollector => IERC20 feeToken) internal _feeToken ;
 
     event FeeTokenChanged(address indexed feeCollector, address oldFeeToken, address newFeeToken) ;
     event FeeCharged(address indexed feeCollector, address indexed feePayer,address indexed feeTokenCharged, uint256 feeAmount) ;
 
+    function defaultFeeToken() public view returns (IERC20) {
+        return _defaultFeeToken ;
+    }
+
     function feeToken(address feeCollector) public view returns (IERC20) {
-        return _feeToken[feeCollector] ;
+        IERC20 token = _feeToken[feeCollector] ;
+        if (address(token) != address(0) ) return token ;
+        return _defaultFeeToken ;
     }
 
     function _setFeeToken(address feeCollector, address newFeeToken) internal {
