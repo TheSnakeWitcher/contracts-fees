a1f87c57cd24363bfd3a37b603ef98b9a099b198
Mr.PAPI <thesnakewitcher@gmail.com>
2023-12-24 15:34:18 -0500

    fix: FeeManagerComponentAmount static contract name

--- contracts/components/FeeManagerComponentAmountStatic.sol
+++ contracts/components/FeeManagerComponentAmountStatic.sol
@@ -6,20 +6,20 @@ pragma abicoder v2;
 /**
  * @title FeeManagerComponentAmountStatic
  */
-abstract contract FeeManagerComponentValueAmountStatic {
+abstract contract FeeManagerComponentAmountStatic {
 
-    mapping(address feeCollector => uint256 feeAmount) private feeAmount ;
+    mapping(address feeCollector => uint256 feeAmount) private _feeAmount ;
 
     event FeeAmountChanged(address feeCollector, uint256 oldFeeAmount, uint256 newFeeAmount) ;
 
     /// @dev maybe call it feesOf ?
     function feeAmount(address feeCollector) public view virtual returns (uint256) {
-        return feeAmount[feeCollector] ;
+        return _feeAmount[feeCollector] ;
     }
 
     function _setFeeAmount(address feeCollector, uint256 newFeeAmount) internal virtual {
         uint256 oldFeeAmount = feeAmount(feeCollector) ;
-        feeAmount[feeCollector] = newFeeAmount ;
+        _feeAmount[feeCollector] = newFeeAmount ;
         emit FeeAmountChanged(feeCollector, oldFeeAmount, newFeeAmount) ;
     }
 
