4cbfbb29fdfada6ca852520a3b8eb6dd8697236c
Mr.PAPI <thesnakewitcher@gmail.com>
2023-12-24 16:01:13 -0500

    fix: import IERC20 in FeeManagerERC20

--- contracts/FeeManagerERC20.sol
+++ contracts/FeeManagerERC20.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.23 ;
 pragma abicoder v2;
 
 
-import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol" ;
+import { SafeERC20, IERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol" ;
 import { FeeManagerComponentERC20 } from "./components/FeeManagerComponentERC20.sol" ;
 import { FeeManagerComponentAmountStatic } from "./components/FeeManagerComponentAmountStatic.sol" ;
 
