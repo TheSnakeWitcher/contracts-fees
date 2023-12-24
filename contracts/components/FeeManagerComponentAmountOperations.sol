002c578de3152f2fbaadee97b62e82a6f1727868
Mr.PAPI <thesnakewitcher@gmail.com>
2023-12-24 15:56:22 -0500

    feat(component): add FeeManagerComponentAmountOperations component

---
+++ contracts/components/FeeManagerComponentAmountOperations.sol
@@ -0,0 +1,56 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23 ;
+pragma abicoder v2;
+
+
+bytes4 constant DEFAULT_OPERATION = bytes4(0) ;
+address constant DEFAULT_FEE_COLLECTOR = address(0) ;
+
+
+/// @title FeeManagerComponentAmountOperations
+abstract contract FeeManagerComponentAmountOperations {
+
+    event FeeAmountChanged(address feeCollector, bytes4 indexed functionSelector, uint256 oldFeeAmount, uint256 newFeeAmount) ;
+    event FeeAmountsChanged(address feeCollector, bytes4[] indexed functionSelectors, uint256[] oldFeeAmounts, uint256[] newFeeAmounts) ;
+
+    mapping(address feeCollector => mapping(bytes4 functionSelector => uint256 feeAmount)) _feeAmounts ;
+
+    constructor(uint256 defaultFeeAmount_) {
+        _feeAmounts[DEFAULT_FEE_COLLECTOR][DEFAULT_OPERATION] = defaultFeeAmount_ ;
+    }
+
+    function feeAmount(address feeCollector, bytes4 functionSelector) public view virtual returns (uint256) {
+        uint256 fee = _feeAmounts[feeCollector][functionSelector] ;
+        if (fee != 0) return fee ;
+
+        fee = _feeAmounts[feeCollector][DEFAULT_OPERATION] ;
+        if ( fee != 0 ) return fee ;
+
+        return _feeAmounts[DEFAULT_FEE_COLLECTOR][DEFAULT_OPERATION];
+    }
+
+    function _setDefaultFeeAmount(address feeCollector, uint256 newFeeAmount) internal virtual {
+        _setFeeAmount(feeCollector, DEFAULT_OPERATION, newFeeAmount) ;
+    }
+
+    function _setFeeAmount(address feeCollector, bytes4 functionSelector, uint256 newFeeAmount) internal virtual {
+        uint256 oldFeeAmount = _feeAmounts[feeCollector][functionSelector] ;
+        _feeAmounts[feeCollector][functionSelector] = newFeeAmount ;
+        emit FeeAmountChanged(feeCollector, functionSelector, oldFeeAmount, newFeeAmount) ;
+    }
+
+    function _setFeeAmounts(address feeCollector, bytes4[] calldata functionSelectors, uint256[] calldata newFeeAmounts) internal virtual {
+        uint256 feeAmountNumber = newFeeAmounts.length ;
+        if ( feeAmountNumber != functionSelectors.length ) revert() ;
+
+        uint256[] memory oldFeeAmounts = new uint256[](feeAmountNumber) ;
+        for(uint i=0 ; i < feeAmountNumber ; ++i ) {
+            bytes4 currentFunctionSelector  = functionSelectors[i] ;
+            oldFeeAmounts[i] = feeAmount(feeCollector, currentFunctionSelector) ;
+            _feeAmounts[feeCollector][currentFunctionSelector] = newFeeAmounts[i];
+        }
+
+        emit FeeAmountsChanged(feeCollector, functionSelectors, oldFeeAmounts, newFeeAmounts) ;
+    }
+
+}
