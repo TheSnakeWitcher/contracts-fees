7d73971702867be20e0afb40943ee2abfa3f43ba
Mr.PAPI <thesnakewitcher@gmail.com>
2023-12-24 13:00:31 -0500

    fix: FeeManagerValue use correct components

--- contracts/FeeManagerValue.sol
+++ contracts/FeeManagerValue.sol
@@ -5,10 +5,10 @@ pragma abicoder v2;
 
 import "@openzeppelin/contracts/utils/Address.sol" ;
 import { FeeManagerComponentValue } from "./components/FeeManagerComponentValue.sol" ;
-import { FeeManagerComponentValueAmountStatic } from "./components/FeeManagerComponentValueAmountStatic.sol" ;
+import { FeeManagerComponentAmountStatic } from "./components/FeeManagerComponentAmountStatic.sol" ;
 
 
-contract FeeManagerValue is FeeManagerComponentValue, FeeManagerComponentValueAmountStatic {
+contract FeeManagerValue is FeeManagerComponentValue, FeeManagerComponentAmountStatic {
 
     using Address for address payable ;
 
