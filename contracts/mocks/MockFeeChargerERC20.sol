0eed981f1cde78de8455926e70f050eb4abae47f
Mr.PAPI <thesnakewitcher@gmail.com>
2023-12-25 17:37:57 -0500

    test: add MockFeeChargerERC20 mock contract

---
+++ contracts/mocks/MockFeeChargerERC20.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.23;
+
+
+import { FeeChargerERC20 } from "../FeeChargerERC20.sol";
+
+
+contract MockFeeChargerERC20 is FeeChargerERC20 {
+
+    constructor(address feeToken_, uint256 feeAmount_) FeeChargerERC20(feeToken_, feeAmount_) {}
+
+    function chargeFees(address feeReceiver) public {
+        _chargeFees(feeReceiver);
+    }
+
+}
