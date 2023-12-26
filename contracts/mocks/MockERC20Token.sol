74c97f0cde8681cdd43a955284138759a434dff6
Mr.PAPI <thesnakewitcher@gmail.com>
2023-12-25 21:33:21 -0500

    test: add MockERC20Token mock contract

---
+++ contracts/mocks/MockERC20Token.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23 ;
+pragma abicoder v2 ;
+
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+/// @title MockERC20Token
+contract MockERC20Token is ERC20 {
+
+  constructor(string memory name_, string memory symbol_, address owner, uint256 initialBalance) ERC20(name_, symbol_) {
+      _mint(owner, initialBalance);
+  }
+
+}
