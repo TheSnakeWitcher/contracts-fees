6b65a861cdaf6a5be3666fb97db6af2f12b00e1b
Mr.PAPI <thesnakewitcher@gmail.com>
2024-03-03 14:31:04 -0500

    test: expose setFeeAmount() in MockFeeChargerERC20

--- contracts/mocks/MockFeeChargerERC20.sol
+++ contracts/mocks/MockFeeChargerERC20.sol
@@ -9,6 +9,10 @@ contract MockFeeChargerERC20 is FeeChargerERC20 {
 
     constructor(address feeToken_, uint256 feeAmount_) FeeChargerERC20(feeToken_, feeAmount_) {}
 
+    function setFeeAmount(uint256 newFeeAmount) public payable {
+        _setFeeAmount(newFeeAmount);
+    }
+
     function chargeFees(address feeReceiver) public payable {
         _chargeFees(feeReceiver);
     }
