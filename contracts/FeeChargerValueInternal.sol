6f431db103ceb39b6d976e435ba3bcf54fc45eac
Mr.PAPI <thesnakewitcher@gmail.com>
2024-03-11 11:44:25 -0400

    fix: use FeeCharger components for FeeChargerValueInternal

--- contracts/FeeChargerValueInternal.sol
+++ contracts/FeeChargerValueInternal.sol
@@ -6,30 +6,30 @@ pragma abicoder v2;
 import "@openzeppelin/contracts/utils/Address.sol" ;
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol" ;
 
-import { FeeManagerComponentValue } from "./components/FeeManagerComponentValue.sol" ;
-import { FeeManagerComponentAmountStatic } from "./components/FeeManagerComponentAmountStatic.sol" ;
+import { FeeChargerComponentValue } from "./components/FeeChargerComponentValue.sol" ;
+import { FeeChargerComponentAmountStatic } from "./components/FeeChargerComponentAmountStatic.sol" ;
 
 
-contract FeeChargerValueInternal is FeeManagerComponentValue, FeeManagerComponentAmountStatic, ReentrancyGuard {
+contract FeeChargerValueInternal is FeeChargerComponentValue, FeeChargerComponentAmountStatic, ReentrancyGuard {
 
     using Address for address payable ;
 
-    constructor(uint256 defaultFeeAmount_) FeeManagerComponentAmountStatic(defaultFeeAmount_) {}
+    constructor(uint256 fee) FeeChargerComponentAmountStatic(fee) {}
 
     /**
      * @dev Non `reentrancy-safe` and non `address(0)-destination-safe`. Must be called in a
      *      external or public payable function.All ether left over at the end of transaction
      *      when calling a payable function is keep in the called contract
      */ 
-    function _chargeFees(address feeCollector, address feePayer) internal virtual {
-        uint256 fee = feeAmount(feeCollector) ;
+    function _chargeFees(address feePayer) internal virtual {
+        uint256 fee = feeAmount() ;
         _checkValue(fee) ;
-        emit FeeCharged(feeCollector, feePayer, fee) ;
+        emit FeeCharged(feePayer, fee) ;
     }
 
     /// @dev Non `address(0)-destination-safe`
     function _withdraw(address payable recipient, uint256 amount) internal virtual nonReentrant {
-        Address.sendValue(recipient, amount);
+        Address.sendValue(recipient, amount); // @NOTE: use PAY opcode when available 
     }
 
 }
